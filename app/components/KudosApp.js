23bc0893888e3329b5f05edb571603c239510c49
Sahil Bajaj <sahil29@gmail.com>
2015-03-13 00:03:12 +0800

    [KudosApp] Begin KudosApp ES6 translation
    - Used arrow functions (no more .bind(this)!)
    - Also hard wrapped long lines

--- app/components/KudosApp.js
+++ app/components/KudosApp.js
@@ -18,55 +18,55 @@ var KudosApp = React.createClass({
 
 	componentDidMount: function () {
 		// Check if the user is already signed in
-		dpd.users.me(function (user) {
+		dpd.users.me((user) => {
 			if (user) {
 				this.setState({
 					currentUser: user
 				});
 			}
-		}.bind(this));
+		});
 
 		// Get the data (kudos and user details) for all users
-		dpd.users.get(function (users) {
+		dpd.users.get((users) => {
 			this.setState({
 				userData: users
 			});
-		}.bind(this));
+		});
 
 		// Update the data when a new kudo is created (either locally or from
 		// another instance)
-		dpd.on('kudos:created', function (kudo) {
-			dpd.users.get(function (users) {
+		dpd.on('kudos:created', (kudo) => {
+			dpd.users.get((users) => {
 				this.setState({
 					userData: users
 				});
-			}.bind(this));
-		}.bind(this));
+			});
+		});
 	},
 
 	handleLogin: function (username, password) {
 		dpd.users.login({
 			username: username,
 			password: password
-		}, function (res, err) {
+		}, (res, err) => {
 			if (err) throw err;
 
-			dpd.users.me(function (user) {
+			dpd.users.me((user) => {
 				this.setState({
 					currentUser: user
 				});
-			}.bind(this));
-		}.bind(this));
+			});
+		});
 	},
 
 	handleLogout: function () {
-		dpd.users.logout(function (res, err) {
+		dpd.users.logout((res, err) => {
 			if (err) throw err;
 
 			this.setState({
 				currentUser: null
 			});
-		}.bind(this));
+		});
 	},
 
 	handleHideBox: function (event) {
@@ -101,12 +101,16 @@ var KudosApp = React.createClass({
 				<header>
 					<h1>Kudos</h1>
 					<div className="current-user-badge">
-						<CurrentUserBox user={this.state.currentUser} handleLogout={this.handleLogout}/>
+						<CurrentUserBox user={this.state.currentUser}
+							handleLogout={this.handleLogout}/>
 					</div>
 				</header>
 				<main>
-					{!this.state.currentUser && <LoginForm handleLogin={this.handleLogin}/>}
-					{this.state.currentUser && <TeamGraph data={this.state.userData} handleLeafNodeClick={this.handleLeafNodeClick}/>}
+					{!this.state.currentUser &&
+						<LoginForm handleLogin={this.handleLogin}/>}
+					{this.state.currentUser &&
+						<TeamGraph data={this.state.userData}
+							handleLeafNodeClick={this.handleLeafNodeClick}/>}
 					<aside>
 						<KudosBox data={this.state.userData}
 							selectedUser={this.state.selectedUser}
