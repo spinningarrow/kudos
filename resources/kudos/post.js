ca696e59ee7f36b0cbd700d0d1c37189dfbc29c3
Elysia Ong <elysia.owx@gmail.com>
2015-05-29 14:17:28 +0800

    Update package.json to include dpd-email
    - Added email sending to kudos post request

--- resources/kudos/post.js
+++ resources/kudos/post.js
@@ -1,4 +1,20 @@
-//dpd.users.get(function (users) {
-  //  emit('kudos:created', users);
-//});
+dpd.users.get({
+    username: this.author
+}, function (authors) {
+    dpd.users.get({
+        username: this.recipient
+    }, function (recipients) {
+        var author = authors[0];
+        var recipient = recipients[0];
+        
+        dpd.email.post({
+            to: recipient.email,
+            subject: 'You\'ve got KUDOS from ' + author.fullname + '!',
+            text: 'Here is your KUDOS:\n' + this.text
+        }, function() {
+            console.log('Email sent to: ' + recipient.fullname + ' <' + recipient.email + '>');
+        });        
+    });
+});
+
 emit('kudos:created', this);
