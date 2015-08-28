342d2e7584554d70d106d6a4ecb5cabc4053ada1
Elysia Ong <elysia.owx@gmail.com>
2015-08-28 14:11:17 +0800

    [KudosNote] Use ReactEmoji as a mixin

--- src/components/KudosNote.js
+++ src/components/KudosNote.js
@@ -3,6 +3,10 @@ let ReactEmoji = require('react-emoji');
 let moment = require('moment');
 
 module.exports = React.createClass({
+	mixins: [
+		ReactEmoji
+	],
+
 	render() {
 		let isoDate = this.props.date;
 		let fullDate = moment(isoDate).format('ddd, D MMM YYYY [at] h:mma');
@@ -11,8 +15,7 @@ module.exports = React.createClass({
 		return (
 			<div className="kudo">
 				<span className="kudo-author">{this.props.author}</span>
-				<span className="kudo-text">{this.props.children}</span>
-				<span className="kudo-text">{ReactEmoji.emojify(this.props.children)}</span>
+				<span className="kudo-text">{this.emojify(this.props.children)}</span>
 				<time className="kudo-date" datetime={isoDate} title={fullDate}>
 					{relativeDate}
 				</time>
