aa0cfb390f5a9eedee729f121d7176e75d726d7a
Sahil Bajaj <sahil29@gmail.com>
2015-09-13 00:01:03 +0800

    Format email code
    - Fixed indentation and formatting
    - Changed "KUDOS" to "kudos"
    
    TODO: The email stuff should probably go in a different file
    TODO: Look into whether email is causing performance issues

--- resources/kudos/post.js
+++ resources/kudos/post.js
@@ -1,42 +1,45 @@
 emit('kudos:created', this);
 
 function escapeHtml(unsafe) {
-    return unsafe
-        .replace(/&/g, '&amp;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;')
-        .replace(/"/g, '&quot;')
-        .replace(/'/g, '&#039;');
+	return unsafe
+		.replace(/&/g, '&amp;')
+		.replace(/</g, '&lt;')
+		.replace(/>/g, '&gt;')
+		.replace(/"/g, '&quot;')
+		.replace(/'/g, '&#039;');
 }
 
-dpd.users.get({
-    username: this.author
-}, function (authors) {
-    dpd.users.get({
-        username: this.recipient
-    }, function (recipients) {
-        var author = authors[0];
-        var recipient = recipients[0];
-        
-        dpd.email.post({
-            to: recipient.email,
-            subject: 'You\'ve got KUDOS from ' + author.fullname + '!',
-            text: 'Here is your KUDOS:\n' + this.text,
-            html: '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' +
-                '<html xmlns="http://www.w3.org/1999/xhtml"><head>' +
-                '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
-                '<title>KUDOS FOR YOU!</title>' +
-                '<meta name="viewport" content="width=device-width, initial-scale=1.0"/>' +
-                '</head><body>' +
-                '<table><tr style="text-align: center;"><td>' +
-                '<h2 style="line-height: 1.2;">You\'ve got KUDOS from <span style="color: #1BADE8;">' + author.fullname + '</span>!</h2>' +
-                '<div style="background: #E1F5FC; padding: 10px;">' + escapeHtml(this.text) + '</div>' +
-                '<div style="margin-top: 1em"><a href="http://kudos.ecs.ads.autodesk.com/">Check it out here!</a></td></tr></table>' +
-                '</body></html>'
-        }, function(result, error) {
-            console.log('Result:', result);
-            console.log('Error', error);
-            console.log('Email sent to: ' + recipient.fullname + ' <' + recipient.email + '>');
-        });
-    });
+dpd.users.get({ username: this.author }, function (authors) {
+	dpd.users.get({ username: this.recipient }, function (recipients) {
+		var author = authors[0];
+		var recipient = recipients[0];
+		var subject = 'You\'ve got kudos from ' + author.fullname + '!';
+		var text = 'Here are your kudos:\n' + this.text;
+		var html = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">' +
+			'<html xmlns="http://www.w3.org/1999/xhtml"><head>' +
+			'<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
+			'<title>You\'ve got kudos!</title>' +
+			'<meta name="viewport" content="width=device-width, initial-scale=1.0"/>' +
+			'</head><body>' +
+			'<table><tr style="text-align: center;"><td>' +
+			'<h2 style="line-height: 1.2;">You\'ve got kudos from <span style="color: #1BADE8;">' + author.fullname + '</span>!</h2>' +
+			'<div style="background: #E1F5FC; padding: 10px;">' + escapeHtml(this.text) + '</div>' +
+			'<div style="margin-top: 1em"><a href="http://kudos.ecs.ads.autodesk.com/">Check it out here!</a></td></tr></table>' +
+			'</body></html>';
+
+		dpd.email.post({
+			to: 'sahil29@gmail.com',
+			subject: subject,
+			text: text,
+			html: html
+		}, function(result, error) {
+			if (error) {
+				console.log('Email error', error);
+				return;
+			}
+
+			console.log('Result', result);
+			console.log('Email sent to: ' + recipient.fullname + ' <' + recipient.email + '>');
+		});
+	});
 });
